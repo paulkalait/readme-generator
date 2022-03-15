a85e9b9e97a92f2e8ab5a141306acd99343552be
Your Name <kalaitzidispaul@gmail.com>
2022-03-15 11:27:35 -0400

    created prompt questions for readme

---
+++ Develop/index.js
@@ -0,0 +1,62 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const fs = require('fs')
+const utils = require('utils')
+
+// const generageMarkdown = require('./utils/generateMarkdown')
+
+// TODO: Create an array of questions for user input
+const questions = [{
+    type: "input",
+    name: "Title",
+    message: "what is the title of your project"
+    
+},
+{
+    type: "type",
+    name: "usernme",
+    message: "What is your github username?"
+},
+{
+    type: "input",
+    name: "email",
+    message: "What is your email address?"
+},
+{
+    type: "input",
+    name: "description",
+    message: "please provide a brief description of your project"
+},
+{
+    type: "checkbox",
+    name: "License",
+    choices: ['MIT', 'BSD 3-Clause', 'Apache 2.0', 'GNU v3.0', 'Mozilla', 'None']
+},
+{   type: "input",
+    name: "Installation",
+    message: "What command must be entered to install dependencies?"
+},
+{   type: "input",
+    name: "Tests",
+    message: "What command must be entered to run tests"
+},
+{   type: "input",
+    name: "usage",
+    message: "What must the user know for usage of repository?"
+},
+{   type: "input",
+    name: "contribution",
+    message: "provide details of contributing by the user"
+}
+
+
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
